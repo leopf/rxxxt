d3ab732e60f2a1553729624ca6bb8193a10691a2
leopf <l.pfob@3-klicks.de>
2025-09-11 09:35:38 +0000

    lifecycle testing

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
 import unittest, typing
 from rxxxt.elements import El
 from rxxxt.component import Component
 from rxxxt.events import NavigateOutputEvent
 from rxxxt.page import default_page
 from rxxxt.session import Session, SessionConfig
-from rxxxt.state import JWTStateResolver, local_state_box
+from rxxxt.state import JWTStateResolver, local_state, local_state_box
 
 session_config = SessionConfig(page_facotry=default_page, state_resolver=JWTStateResolver(b"deez"), persistent=False)
 
@@ -63,6 +64,49 @@ class TestSession(unittest.IsolatedAsyncioTestCase):
         await session.update()
       update = await session.render_update(True, True)
       self.assertIn("yes", update.html_parts[0])
+
+  async def test_lifecycle(self):
+    testobj = self
+    counters: defaultdict[str, int] = defaultdict(int)
+    class Main(Component):
+      async def on_init(self) -> None:
+        testobj.assertEqual(counters["before_destroy"], 0)
+        counters["init"] += 1
+      async def on_before_update(self) -> None:
+        testobj.assertEqual(counters["render"], counters["before_update"])
+        counters["before_update"] += 1
+      def render(self):
+        self.context.request_update()
+        counters["render"] += 1
+        testobj.assertEqual(counters["render"], counters["before_update"])
+        return El.div()
+      async def on_after_update(self) -> None:
+        counters["after_update"] += 1
+        testobj.assertEqual(counters["after_update"], counters["render"])
+      async def on_before_destroy(self) -> None:
+        testobj.assertEqual(counters["init"], 1)
+        testobj.assertEqual(counters["before_destroy"], 0)
+        counters["before_destroy"] += 1
+      async def on_after_destroy(self) -> None:
+        counters["after_destroy"] += 1
+        testobj.assertEqual(counters["after_destroy"], counters["before_destroy"])
+
+    class Switcher(Component):
+      hidden = local_state(bool)
+      def render(self):
+        if self.hidden: return El.div()
+        else: return Main()
+
+    el = Switcher()
+    async with Session(session_config, el) as session:
+      session.set_location("/")
+      await session.init(None)
+      await session.update()
+      self.assertEqual(counters["render"], 2)
+      self.assertEqual(counters["after_destroy"], 0)
+      el.hidden = True
+      await session.update()
+      self.assertEqual(counters["after_destroy"], 1)
 
 if __name__ == "__main__":
   _ = unittest.main()
