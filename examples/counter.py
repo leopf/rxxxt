e52c32b2a2df0b0340815ff2160820d67bffc5d0
leopf <43857362+leopf@users.noreply.github.com>
2025-10-30 22:23:07 +0100

    docs cleanup (#24)

--- examples/counter.py
+++ examples/counter.py
@@ -1,11 +1,12 @@
-from rxxxt import Component, event_handler, El, Element, App, local_state
 import uvicorn
+from rxxxt import Component, event_handler, El, Element, App, local_state
 
 class Counter(Component):
   count = local_state(int)
 
   @event_handler()
-  def on_click(self): self.count += 1
+  def on_click(self):
+    self.count += 1
 
   def render(self) -> Element:
     return El.div(onclick=self.on_click, content=[f"Count: {self.count}"])
